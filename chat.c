62a0e45b935d8a0ed0538019ac0803e530f04e00
Phi Hiep <phihiepnguyen.vn@gmail.com>
2014-12-08 08:06:12 +0700

    add sendfile module

--- chat.c
+++ chat.c
@@ -154,7 +154,6 @@ int main(int argc , char *argv[])
         {
             fwrite(buffer, sizeof(char), len, received_file);
             remain_data -= len;
-            fprintf(stdout, "Receive %d bytes and we hope :- %d bytes\n", len, remain_data);
             write(socket, "OK", 2);
         }
     }
